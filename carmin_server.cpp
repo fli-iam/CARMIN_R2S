f1d3b2a5dbda592444d89b262510ea286fd3190f
JinpengLI <mr.li.jinpeng@gmail.com>
2016-09-22 14:26:41 +0200

    add Access-Control-Allow-Origin

--- carmin_server.cpp
+++ carmin_server.cpp
@@ -25,9 +25,183 @@
 
 Config config = Config();
 
+int (* old_freponse)(struct soap *soap, int soap_error_code, size_t count);
+
+static const char soap_padding[4] = "\0\0\0";
+#define SOAP_STR_EOS (soap_padding)
+
+static const struct soap_code_map h_http_error_codes[] =
+{ { 200, "OK" },
+  { 201, "Created" },
+  { 202, "Accepted" },
+  { 203, "Non-Authoritative Information" },
+  { 204, "No Content" },
+  { 205, "Reset Content" },
+  { 206, "Partial Content" },
+  { 300, "Multiple Choices" },
+  { 301, "Moved Permanently" },
+  { 302, "Found" },
+  { 303, "See Other" },
+  { 304, "Not Modified" },
+  { 305, "Use Proxy" },
+  { 307, "Temporary Redirect" },
+  { 400, "Bad Request" },
+  { 401, "Unauthorized" },
+  { 402, "Payment Required" },
+  { 403, "Forbidden" },
+  { 404, "Not Found" },
+  { 405, "Method Not Allowed" },
+  { 406, "Not Acceptable" },
+  { 407, "Proxy Authentication Required" },
+  { 408, "Request Time-out" },
+  { 409, "Conflict" },
+  { 410, "Gone" },
+  { 411, "Length Required" },
+  { 412, "Precondition Failed" },
+  { 413, "Request Entity Too Large" },
+  { 414, "Request-URI Too Large" },
+  { 415, "Unsupported Media Type" },
+  { 416, "Requested range not satisfiable" },
+  { 417, "Expectation Failed" },
+  { 500, "Internal Server Error" },
+  { 501, "Not Implemented" },
+  { 502, "Bad Gateway" },
+  { 503, "Service Unavailable" },
+  { 504, "Gateway Time-out" },
+  { 505, "HTTP Version not supported" },
+  {   0, NULL }
+};
+
+
+
+static const char*
+http_error(struct soap *soap, int status)
+{ register const char *msg = SOAP_STR_EOS;
+#ifndef WITH_LEAN
+  msg = soap_code_str(h_http_error_codes, status);
+  if (!msg)
+    msg = SOAP_STR_EOS;
+#endif
+  return msg;
+}
+
+
+
+int soap_fresponse(struct soap *soap, int status, size_t count)
+{
+  //int err;
+  //std::cout << "DEBUG soap_fresponse" << std::endl;
+
+  //if ((err = soap->fposthdr(soap, "Access-Control-Allow-Origin", "*")))
+  //    return err;
+  //if ((err = soap->fposthdr(soap, "Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, CONNECT")))
+  //    return err;
+  //if ((err = soap->fposthdr(soap, "Access-Control-Allow-Headers", "X-Requested-With, Content-Type")))
+  //    return err;
+  //if ((err = soap->fposthdr(soap, "Access-Control-Allow-Credentials", "true")))
+  //    return err;
+   
+  //int ret = old_freponse(soap, soap_error_code, count);
+
+  //return ret;
+
+register int err;
+#ifdef WMW_RPM_IO
+  if (soap->rpmreqid)
+    httpOutputEnable(soap->rpmreqid);
+#endif
+  if (strlen(soap->http_version) > 4)
+    return soap->error = SOAP_EOM;
+  if (!status || status == SOAP_HTML || status == SOAP_FILE)
+  { const char *s;
+    if (count || ((soap->omode & SOAP_IO) == SOAP_IO_CHUNK))
+      s = "200 OK";
+    else
+      s = "202 ACCEPTED";
+    DBGLOG(TEST, SOAP_MESSAGE(fdebug, "Status = %s\n", s));
+#ifdef WMW_RPM_IO
+    if (soap->rpmreqid || soap_valid_socket(soap->master) || soap_valid_socket(soap->socket)) /* RPM behaves as if standalone */
+#else
+    if (soap_valid_socket(soap->master) || soap_valid_socket(soap->socket)) /* standalone application (socket) or CGI (stdin/out)? */
+#endif
+    { sprintf(soap->tmpbuf, "HTTP/%s %s", soap->http_version, s);
+      if ((err = soap->fposthdr(soap, soap->tmpbuf, NULL)))
+        return err;
+    }
+    else if ((err = soap->fposthdr(soap, "Status", s))) /* CGI header */
+      return err;
+  }
+  else if (status >= 200 && status < 600)
+  { sprintf(soap->tmpbuf, "HTTP/%s %d %s", soap->http_version, status, http_error(soap, status));
+    if ((err = soap->fposthdr(soap, soap->tmpbuf, NULL)))
+      return err;
+#ifndef WITH_LEAN 
+    if (status == 401)
+    { sprintf(soap->tmpbuf, "Basic realm=\"%s\"", (soap->authrealm && strlen(soap->authrealm) < sizeof(soap->tmpbuf) - 14) ? soap->authrealm : "gSOAP Web Service");
+      if ((err = soap->fposthdr(soap, "WWW-Authenticate", soap->tmpbuf)))
+        return err;
+    }
+    else if ((status >= 301 && status <= 303) || status == 307)
+    { if ((err = soap->fposthdr(soap, "Location", soap->endpoint)))
+        return err;
+    }
+#endif
+  }
+else
+  { const char *s = *soap_faultcode(soap);
+    if (status >= SOAP_GET_METHOD && status <= SOAP_HTTP_METHOD)
+      s = "405 Method Not Allowed";
+    else if (soap->version == 2 && (!s || !strcmp(s, "SOAP-ENV:Sender")))
+      s = "400 Bad Request";
+    else
+      s = "500 Internal Server Error";
+    DBGLOG(TEST, SOAP_MESSAGE(fdebug, "Error %s (status=%d)\n", s, status));
+#ifdef WMW_RPM_IO
+    if (soap->rpmreqid || soap_valid_socket(soap->master) || soap_valid_socket(soap->socket)) /* RPM behaves as if standalone */
+#else
+    if (soap_valid_socket(soap->master) || soap_valid_socket(soap->socket)) /* standalone application */
+#endif
+    { sprintf(soap->tmpbuf, "HTTP/%s %s", soap->http_version, s);
+      if ((err = soap->fposthdr(soap, soap->tmpbuf, NULL)))
+        return err;
+    }
+    else if ((err = soap->fposthdr(soap, "Status", s))) /* CGI */
+      return err;
+  }
+  if ((err = soap->fposthdr(soap, "Server", "gSOAP/2.7"))
+   || (err = soap_puthttphdr(soap, status, count)))
+    return err;
+#ifdef WITH_COOKIES
+  if (soap_putsetcookies(soap))
+    return soap->error;
+#endif
+
+  if ((err = soap->fposthdr(soap, "Access-Control-Allow-Origin", "*")))
+      return err;
+
+  return soap->fposthdr(soap, NULL, NULL);
+
+
+}
+
+int soap_fhead(struct soap *soap)
+{
+   //std::cout << "DEBUG soap_fhead" << std::endl;
+   return SOAP_OK;
+}
+
+int http_post(struct soap *soap, const char *endpoint, const char *host, int port, const char *path, const char *action, size_t count)
+{
+    //std::cout << "hi, i am here post" << std::endl;
+    //soap->fposthdr(soap, "Access-Control-Allow-Origin", "*"); 
+    return SOAP_OK;
+}
 
 int http_get(struct soap *soap) 
-{ 
+{
+   //std::cout << "DEBUG i am here get" << std::endl;
+   //soap->fposthdr(soap, "Access-Control-Allow-Origin", "*");
+
    FILE *fd = NULL;
    char *s = strchr(soap->path, '?'); 
    if (!s || strcmp(s, "?wsdl")) 
@@ -57,6 +231,13 @@ void *process_request(void *soap)
    ((struct soap*)soap)->send_timeout = 60; // Timeout after 1 minute stall on send 
 
    ((struct soap*)soap)->fget = http_get;
+   ((struct soap*)soap)->fpost = http_post;
+   old_freponse = ((struct soap*)soap)->fresponse;
+   ((struct soap*)soap)->fresponse = soap_fresponse;
+
+   ((struct soap*)soap)->fhead = soap_fhead;
+
+
    soap_serve((struct soap*)soap);
  
    soap_destroy((struct soap*)soap); 
